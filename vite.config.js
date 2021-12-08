eff01384fe1fbd6c88c7c1691ebd6adbc1aeabf5
ViliusP <viliuspaliokas@gmail.com>
2021-12-08 11:39:18 +0200

    Fix page loading

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  plugins: [vue()],
   server: {
     port: 8080 // <- change server port here
   },
